18ffb17c465761fc314fe3aac34b6d8cc11e8f70
hojun <hjlee9303@gmail.com>
2023-07-19 16:49:12 +0900

    Feat: 주문과 할인 도메인 개발  - 주문, 할인 인터페이스 추가  - 주문, 할인 구현체 구현체 추가  - junit 테스트 추가

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmout = 1000;
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmout;
+        }
+        return 0;
+    }
+}
